3c570e74dffcfe8b1ddb2a81be84ba83b7d42d77
Thomas Bercht <thomas.bercht@heig-vd.ch>
2021-10-12 13:40:10 +0200

    change title

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ $items = $db->query($selectQuery);
 
 <html>
   <head>
-    <title>TodoList</title>
+    <title>nouveau titre</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
